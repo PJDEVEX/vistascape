77a3cd40472b46dbc20911ef3b7c4de4d1e2bbc2
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-09 21:15:51 +0000

    Add and CURD fun to comments app

--- comments/views.py
+++ comments/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics, permissions
-from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
@@ -7,21 +6,23 @@ from .serializers import CommentSerializer, CommentDetailSerializer
 
 class CommentList(generics.ListCreateAPIView):
     """
-    List comments or create a comment if logged in.
+    List all comments
+    Create a new comment if authenticated
+    Associate the current logged in user with the comment
     """
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = Comment.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['post']
 
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = CommentSerializer
+    queryset = Comment.objects.all()
+    
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    Retrieve a comment, or update or delete it by id if you own it.
+    Retrieve a comment
+    update or delete a comment if owner
     """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
--- drf_api/settings.py
+++ drf_api/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     # Custom apps
     "profiles",
     "posts",
+    "comments",
 ]
 
 MIDDLEWARE = [
