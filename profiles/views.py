608ee0435cff5976428cb4296d09f1dbf3a8b183
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-09 15:08:44 +0000

    Add profile detail view

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,25 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from django.http import Http404
+
 
 class ProfileList(APIView):
     def get(self, request):
-        profiles = Profile.objects.all()     
-        serializer = ProfileSerializer(profiles, many= True)
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
         return Response(serializer.data)
 
+
+class ProfileDetail(APIView):
+    def get_object(self, pk):
+        try:
+            profile = Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
