9779b4959fc40ce6686820c07bc0e369f66346c3
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-09 15:09:38 +0000

    Add profile detail view

--- profiles/views.py
+++ profiles/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from django.http import Http404
+from rest_framework import status
 
 
 class ProfileList(APIView):
