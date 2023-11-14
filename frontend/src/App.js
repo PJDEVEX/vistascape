8ba5d109844d4446f48f7fcc2bbc62811826af40
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-14 23:54:11 +0000

    USER STORY: #40 - Add Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import Button from 'react-bootstrap/Button'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className='App'>
       <Button variant='primary'>It works!</Button>
+      <NavBar />
     </div>
   );
 }
