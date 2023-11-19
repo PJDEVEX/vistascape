fbdb23e6869e8b60abc7e4d2e6f81d39f2b8ad50
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-19 19:50:27 +0000

    Test: Config frontend test

---
+++ frontend/src/mocks/handlers.js
@@ -0,0 +1,22 @@
+// src/mocks/handlers.js
+import { rest } from "msw";
+
+export const handlers = [
+  rest.get("/dj-rest-auth/user/", (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 3,
+        username: "liam",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 3,
+        profile_image: "https://res.cloudinary.com/pjdevex/image/upload/v1/media/images/Reflection_yoi9el",
+      })
+    );
+  }),
+
+  rest.post("/dj-rest-auth/logout/", (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+import {setupserver} from ",msw/node";
+import { handlers } from "./mocks/handlers";
+
+const server = setupserver(...handlers)
+
+// Start server before all test
+beforeAll(() => server.listen());
+// Reste after each
+afterEach(() => server.resetHandlers());
+// Close it after all text
+afterAll(() => server.close());
