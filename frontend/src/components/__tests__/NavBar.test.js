d2632d747ee968fc509e388667460ba23541cfc7
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-22 23:31:30 +0000

    Test: for hiding 'Profile' link in NavBar for logged-in user

--- frontend/src/components/__tests__/NavBar.test.js
+++ frontend/src/components/__tests__/NavBar.test.js
@@ -14,3 +14,22 @@ test("renders NavBar", () => {
   expect(signInLink).toBeInTheDocument();
 });
 
+/**
+ * Test: renders link to the user profile for a logged-in user
+ * Description: Verifies that the NavBar component, wrapped in CurrentUserProvider,
+ *   hides the 'Profile' link when a user is logged in.
+ */
+test("renders link to the user profile for a logged in user", async () => {
+  // Step 1: Render the NavBar component within a Router and CurrentUserProvider.
+  render(
+    <Router>
+      <CurrentUserProvider>
+        <NavBar />
+      </CurrentUserProvider>
+    </Router>
+  );
+
+  // Step 2: Wait for the appearance of the 'Profile' link and assert it is not present.
+  const profileAvatar = await screen.findByText("Profile");
+  expect(profileAvatar).not.toBeInTheDocument();
+});
