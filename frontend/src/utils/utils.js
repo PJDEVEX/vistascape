b642f7fbd3b33226f1681578df57fc98fc230daa
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-22 22:13:02 +0000

    Fix: Improve error handling in fetchMoreData, log detailed info on failure

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -12,8 +12,20 @@ export const fetchMoreData = async (resource, setResource) => {
           : [...acc, cur];
       }, prevResource.results),
     }));
-  } catch (err) {}
+  } catch (error) {
+    if (error.response) {
+      console.error("Server responded with an error:", error.response.status, error.response.data);
+    } else if (error.request) {
+      console.error("No response received from the server. Request made:", error.request);
+    } else {
+      console.error("Error during request setup:", error.message);
+    }
+    console.error("Full error object:", error);
+    // You can rethrow the error or handle it based on your requirements
+    throw error;
+  }
 };
+
 
 export const followHelper = (profile, clickedProfile, following_id) => {
   return profile.id === clickedProfile.id
@@ -25,7 +37,7 @@ export const followHelper = (profile, clickedProfile, following_id) => {
         following_id,
       }
     : profile.is_owner
-      ? // This is the profile of the logged in user
+      ? // This is the profile of the logged-in user
         // update its following count
         { ...profile, following_count: profile.following_count + 1 }
       : // this is not the profile the user clicked on or the profile
@@ -43,7 +55,7 @@ export const unfollowHelper = (profile, clickedProfile) => {
         following_id: null,
       }
     : profile.is_owner
-      ? // This is the profile of the logged in user
+      ? // This is the profile of the logged-in user
         // update its following count
         { ...profile, following_count: profile.following_count - 1 }
       : // this is not the profile the user clicked on or the profile
