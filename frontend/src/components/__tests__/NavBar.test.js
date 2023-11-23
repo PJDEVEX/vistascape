9eb051c0f751e6b48b5a4d8dcc7b5628c4b81539
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-23 00:08:49 +0000

    Test to pass: renders link to the user profile for a logged in user

--- frontend/src/components/__tests__/NavBar.test.js
+++ frontend/src/components/__tests__/NavBar.test.js
@@ -17,11 +17,10 @@ test("renders NavBar", () => {
 
 /**
  * Test: renders link to the user profile for a logged-in user
- * Description: Verifies that the NavBar component, wrapped in CurrentUserProvider,
- *   hides the 'Profile' link when a user is logged in.
+ * Description: Ensures NavBar displays 'Profile' link for a logged-in user.
  */
 test("renders link to the user profile for a logged in user", async () => {
-  // Step 1: Render the NavBar component within a Router and CurrentUserProvider.
+  // Step 1: Render the NavBar component with CurrentUserProvider within a Router component.
   render(
     <Router>
       <CurrentUserProvider>
@@ -30,7 +29,9 @@ test("renders link to the user profile for a logged in user", async () => {
     </Router>
   );
 
-  // Step 2: Wait for the appearance of the 'Profile' link and assert it is not present.
+  // Step 2: Use asynchronous findByText to locate the "Profile" link in the NavBar.
   const profileAvatar = await screen.findByText("Profile");
-  expect(profileAvatar).not.toBeInTheDocument();
+
+  // Step 3: Ensure that the "Profile" link is present in the rendered NavBar.
+  expect(profileAvatar).toBeInTheDocument();
 });
