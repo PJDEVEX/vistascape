be69476edba529ec3feaaf44fcf35d2b28317cea
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-23 18:11:59 +0000

    Test Scenario 24

---
+++ frontend/src/components/__tests__/SignInForm.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import SignInForm from "../../pages/auth/SignInForm";
+
+/**
+ * Test: renders without errors
+ * Description: Verify SignInForm component renders without errors.
+ */
+test("renders without errors", async () => {
+  // Step 1: Arrange - Render the SignInForm component within a MemoryRouter.
+  const { getByText, getByPlaceholderText } = render(
+    <Router>
+      <SignInForm />
+    </Router>
+  );
+
+  // Step 2: Assert - Check if the necessary elements are present to ensure
+  // the component does not render without errors
+  expect(getByText("sign in")).not.toBeInTheDocument();
+  expect(getByPlaceholderText("Username")).toBeInTheDocument();
+  expect(getByPlaceholderText("Password")).toBeInTheDocument();
+});
