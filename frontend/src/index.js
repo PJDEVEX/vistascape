c19ee4d3bf5fbf859728864f6d90530852775da4
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 08:58:37 +0000

    Chro(index): add comments for enhanced readability

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,17 +7,26 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { CurrentUserProvider } from "./contexts/CurrentUserContext";
 import { ProfileDataProvider } from "./contexts/ProfileDataContext";
 
+/**
+ * Entry point of the application. Renders the main App component within
+ * the React Strict Mode for enhanced development experience.
+ */
 ReactDOM.render(
   <React.StrictMode>
+    {/* Wrap the entire application with React Router for client-side routing */}
     <Router>
+      {/* Provide the current user context to the entire application */}
       <CurrentUserProvider>
+        {/* Provide profile data context to the entire application */}
         <ProfileDataProvider>
+          {/* Render the main App component */}
           <App />
         </ProfileDataProvider>
       </CurrentUserProvider>
     </Router>
   </React.StrictMode>,
-  document.getElementById("root"),
+  // Mount the application on the root element in the HTML document
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
