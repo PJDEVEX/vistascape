0ea2d823712e93184e78c29ee7d2ad9cf0d6a164
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 09:55:58 +0000

    Chro(utils): add comments for enhanced readability

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,8 +1,17 @@
 import { axiosReq } from "../api/axiosDefaults";
 
+/**
+ * Fetches more data for a given resource and updates the state.
+ *
+ * @param {object} resource - The current resource state.
+ * @param {function} setResource - The state update function.
+ */
 export const fetchMoreData = async (resource, setResource) => {
   try {
+    // Fetch data from the next endpoint using axios.
     const { data } = await axiosReq.get(resource.next);
+
+    // Update the resource state with the new data, avoiding duplicates.
     setResource((prevResource) => ({
       ...prevResource,
       next: data.next,
@@ -13,52 +22,69 @@ export const fetchMoreData = async (resource, setResource) => {
       }, prevResource.results),
     }));
   } catch (error) {
+    // Handle different types of errors and log details.
     if (error.response) {
-      console.error("Server responded with an error:", error.response.status, error.response.data);
+      console.error(
+        "Server responded with an error:",
+        error.response.status,
+        error.response.data
+      );
     } else if (error.request) {
-      console.error("No response received from the server. Request made:", error.request);
+      console.error(
+        "No response received from the server. Request made:",
+        error.request
+      );
     } else {
       console.error("Error during request setup:", error.message);
     }
     console.error("Full error object:", error);
-    // You can rethrow the error or handle it based on your requirements
+
+    // Rethrow the error or handle it based on requirements.
     throw error;
   }
 };
 
-
+/**
+ * Helper function to update profile data when following a user.
+ *
+ * @param {object} profile - The current user's profile.
+ * @param {object} clickedProfile - The profile of the user being followed.
+ * @param {string} following_id - The id of the followed user.
+ * @returns {object} - The updated profile.
+ */
 export const followHelper = (profile, clickedProfile, following_id) => {
   return profile.id === clickedProfile.id
-    ? // This is the profile I clicked on,
-      // update its followers count and set its following id
+    ? // Update the clicked profile's followers count and following id.
       {
         ...profile,
         followers_count: profile.followers_count + 1,
         following_id,
       }
     : profile.is_owner
-      ? // This is the profile of the logged-in user
-        // update its following count
+      ? // Update the logged-in user's following count.
         { ...profile, following_count: profile.following_count + 1 }
-      : // this is not the profile the user clicked on or the profile
-        // the user owns, so just return it unchanged
+      : // Return unchanged if not the clicked profile or user's own profile.
         profile;
 };
 
+/**
+ * Helper function to update profile data when unfollowing a user.
+ *
+ * @param {object} profile - The current user's profile.
+ * @param {object} clickedProfile - The profile of the user being unfollowed.
+ * @returns {object} - The updated profile.
+ */
 export const unfollowHelper = (profile, clickedProfile) => {
   return profile.id === clickedProfile.id
-    ? // This is the profile I clicked on,
-      // update its followers count and set its following id
+    ? // Update the clicked profile's followers count and reset following id.
       {
         ...profile,
         followers_count: profile.followers_count - 1,
         following_id: null,
       }
     : profile.is_owner
-      ? // This is the profile of the logged-in user
-        // update its following count
+      ? // Update the logged-in user's following count.
         { ...profile, following_count: profile.following_count - 1 }
-      : // this is not the profile the user clicked on or the profile
-        // the user owns, so just return it unchanged
+      : // Return unchanged if not the clicked profile or user's own profile.
         profile;
 };
