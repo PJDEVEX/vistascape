f37186abc2abe1cf35c5f0e67459a64034008beb
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 08:56:07 +0000

    Chro(useRedirect): add comments for enhanced readability

--- frontend/src/hooks/useRedirect.js
+++ frontend/src/hooks/useRedirect.js
@@ -2,25 +2,37 @@ import axios from "axios";
 import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
+/**
+ * Custom hook to handle redirection based on user authentication status.
+ * @param {string} userAuthStatus - The authentication status of the user 
+          * ("loggedIn" or "loggedOut").
+ */
 export const useRedirect = (userAuthStatus) => {
+  // Get the history object from React Router.
   const history = useHistory();
 
   useEffect(() => {
+    /**
+     * Function to be executed on component mount.
+     */
     const handleMount = async () => {
       try {
+        // Make a POST request to refresh the authentication token.
         await axios.post("/dj-rest-auth/token/refresh/");
-        // if user is logged in, the code below will run
+
+        // Redirect to the home page if the user is logged in.
         if (userAuthStatus === "loggedIn") {
           history.push("/");
         }
       } catch (err) {
-        // if user is not logged in, the code below will run
+        // Redirect to the home page if the user is not logged in.
         if (userAuthStatus === "loggedOut") {
           history.push("/");
         }
       }
     };
 
+    // Call the mount handler function.
     handleMount();
   }, [history, userAuthStatus]);
 };
