41138dcadf13fcf9eb3ec8faf77b7ecf61fbdbfd
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 08:14:19 +0000

    Chro(ColorModeToggle): add comments for enhanced readability

--- frontend/src/components/DarkModeToggle.js
+++ frontend/src/components/DarkModeToggle.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { useColorScheme } from '../hooks/useColorScheme';
+import React from "react";
+import { useColorScheme } from "../hooks/useColorScheme";
 import styles from "../styles/DarkModeToggle.module.css";
 
+/**
+ * ColorModeToggle component for toggling between light and dark color schemes.
+ */
 const ColorModeToggle = () => {
+  // Get the current color scheme and its setter function
   const { isDark, setIsDark } = useColorScheme();
 
   // Handle the toggle button click
   const handleToggleClick = () => {
-    setIsDark(!isDark);
+    setIsDark(!isDark); // Toggle the color scheme
   };
 
   // Determine which icon to show based on the color scheme
@@ -20,13 +24,16 @@ const ColorModeToggle = () => {
   // Bug print
   // console.log('isDark:', isDark);
 
+  /**
+   * Render the Font Awesome icon as the toggle button.
+   * Clicking the button toggles between light and dark modes.
+   */
   return (
-    // Render the Font Awesome icon as the toggle
     <div
       onClick={handleToggleClick}
       className={styles.ColorModeToggle}
       data-bs-theme="dark"
-      aria-label='Color mode toggle'
+      aria-label="Color mode toggle"
     >
       {icon}
     </div>
