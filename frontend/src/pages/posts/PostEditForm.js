92ab5fab7affb39aa5a241343e00b8139aca3844
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 09:27:01 +0000

    Chro(PostEditForm): add comments for enhanced readability

--- frontend/src/pages/posts/PostEditForm.js
+++ frontend/src/pages/posts/PostEditForm.js
@@ -1,28 +1,37 @@
+// Importing necessary React and Bootstrap components, styles, and utilities
 import React, { useEffect, useRef, useState } from "react";
-
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Container from "react-bootstrap/Container";
-import Alert from "react-bootstrap/Alert";
-import Image from "react-bootstrap/Image";
+import {
+  Form,
+  Button,
+  Row,
+  Col,
+  Container,
+  Alert,
+  Image,
+} from "react-bootstrap";
 
 import styles from "../../styles/PostCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
+// Importing React Router hooks and Axios utility function
 import { useHistory, useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+
+// Custom hook for detecting color scheme
 import { useColorScheme } from "../../hooks/useColorScheme";
 
+/**
+ * PostEditForm component for editing existing posts.
+ */
 function PostEditForm() {
+  // Destructuring color scheme information
   const { isDark } = useColorScheme();
-  const darkClass = isDark? styles["dark"] : "";
+  const darkClass = isDark ? styles["dark"] : "";
   const appDarkClass = isDark ? appStyles["dark"] : "";
 
+  // State to manage form errors and post data
   const [errors, setErrors] = useState({});
-
   const [postData, setPostData] = useState({
     title: "",
     content: "",
@@ -30,16 +39,19 @@ function PostEditForm() {
   });
   const { title, content, image } = postData;
 
+  // Ref for image input and history object for navigation
   const imageInput = useRef(null);
   const history = useHistory();
   const { id } = useParams();
 
+  // Fetch post data on component mount
   useEffect(() => {
     const handleMount = async () => {
       try {
         const { data } = await axiosReq.get(`/posts/${id}/`);
         const { title, content, image, is_owner } = data;
 
+        // Redirect if the user is not the owner of the post
         is_owner ? setPostData({ title, content, image }) : history.push("/");
       } catch (err) {
         console.log(err);
@@ -49,6 +61,7 @@ function PostEditForm() {
     handleMount();
   }, [history, id]);
 
+  // Event handler for form input changes
   const handleChange = (event) => {
     setPostData({
       ...postData,
@@ -56,6 +69,7 @@ function PostEditForm() {
     });
   };
 
+  // Event handler for changing the image file
   const handleChangeImage = (event) => {
     if (event.target.files.length) {
       URL.revokeObjectURL(image);
@@ -66,6 +80,7 @@ function PostEditForm() {
     }
   };
 
+  // Event handler for form submission
   const handleSubmit = async (event) => {
     event.preventDefault();
     const formData = new FormData();
@@ -73,21 +88,25 @@ function PostEditForm() {
     formData.append("title", title);
     formData.append("content", content);
 
+    // Append image file to form data if selected
     if (imageInput?.current?.files[0]) {
       formData.append("image", imageInput.current.files[0]);
     }
 
     try {
+      // Send PUT request to update the post
       await axiosReq.put(`/posts/${id}/`, formData);
       history.push(`/posts/${id}`);
     } catch (err) {
       console.log(err);
+      // Set form errors if the request fails (excluding unauthorized errors)
       if (err.response?.status !== 401) {
         setErrors(err.response?.data);
       }
     }
   };
 
+  // JSX for text input fields
   const textFields = (
     <div className="text-center">
       <Form.Group>
@@ -123,25 +142,32 @@ function PostEditForm() {
         </Alert>
       ))}
 
+      {/* Cancel and Save buttons */}
       <Button
         className={`${btnStyles.Button} ${btnStyles.Green}`}
         onClick={() => history.goBack()}
       >
         cancel
       </Button>
-      <Button className={`${btnStyles.Button} ${btnStyles.Green}`} type="submit">
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Green}`}
+        type="submit"
+      >
         save
       </Button>
     </div>
   );
 
+  // Render the form with image upload and text input fields
   return (
     <Form onSubmit={handleSubmit}>
       <Row>
+        {/* Responsive layout for different screen sizes */}
         <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
           <Container
             className={`${appStyles.Content} ${styles.Container} ${appDarkClass} d-flex flex-column justify-content-center`}
           >
+            {/* Image preview, image upload button, and image input */}
             <Form.Group className="text-center">
               <figure>
                 <Image className={appStyles.Image} src={image} rounded />
@@ -168,15 +194,21 @@ function PostEditForm() {
               </Alert>
             ))}
 
+            {/* Render textFields for mobile view */}
             <div className="d-md-none">{textFields}</div>
           </Container>
         </Col>
+
+        {/* Display textFields in a separate container for larger screens */}
         <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
-          <Container className={`${appStyles.Content} ${appDarkClass}`}>{textFields}</Container>
+          <Container className={`${appStyles.Content} ${appDarkClass}`}>
+            {textFields}
+          </Container>
         </Col>
       </Row>
     </Form>
   );
 }
 
+// Exporting the PostEditForm component
 export default PostEditForm;
