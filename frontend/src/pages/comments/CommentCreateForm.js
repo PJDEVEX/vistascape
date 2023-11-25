7e50156f60deaa5e42017d83f0afc0f85ae40598
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 09:11:20 +0000

    Chro(CommentCreateForm): add comments for enhanced readability

--- frontend/src/pages/comments/CommentCreateForm.js
+++ frontend/src/pages/comments/CommentCreateForm.js
@@ -9,28 +9,55 @@ import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 import { useColorScheme } from "../../hooks/useColorScheme";
 
+/**
+ * CommentCreateForm component for creating comments on a post.
+ *
+ * @param {Object} props - Component properties.
+ * @param {Object} props.post - The post object.
+ * @param {Function} props.setPost - Function to update the post state.
+ * @param {Function} props.setComments - Function to update the comments state.
+ * @param {string} props.profileImage - URL of the user's profile image.
+ * @param {string} props.profile_id - User's profile ID.
+ */
 function CommentCreateForm(props) {
+  // Retrieve color scheme for styling
   const { isDark } = useColorScheme();
   const darkClass = isDark ? styles["dark"] : "";
 
+  // Destructure props
   const { post, setPost, setComments, profileImage, profile_id } = props;
   const [content, setContent] = useState("");
 
+  /**
+   * Handle change event for the comment input.
+   *
+   * @param {Object} event - The change event object.
+   */
   const handleChange = (event) => {
     setContent(event.target.value);
   };
 
+  /**
+   * Handle submit event for the comment form.
+   *
+   * @param {Object} event - The submit event object.
+   */
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Make a POST request to create a new comment
       const { data } = await axiosRes.post("/comments/", {
         content,
         post,
       });
+
+      // Update the comments state with the new comment
       setComments((prevComments) => ({
         ...prevComments,
         results: [data, ...prevComments.results],
       }));
+
+      // Update the post state to reflect the increased comments count
       setPost((prevPost) => ({
         results: [
           {
@@ -39,6 +66,8 @@ function CommentCreateForm(props) {
           },
         ],
       }));
+
+      // Clear the comment input after submission
       setContent("");
     } catch (err) {
       console.log(err);
@@ -49,9 +78,11 @@ function CommentCreateForm(props) {
     <Form className="mt-2" onSubmit={handleSubmit}>
       <Form.Group>
         <InputGroup>
+          {/* Link to user's profile */}
           <Link to={`/profiles/${profile_id}`}>
             <Avatar src={profileImage} />
           </Link>
+          {/* Comment input */}
           <Form.Control
             className={`${styles.Form} ${darkClass}`}
             placeholder="my comment..."
@@ -62,6 +93,7 @@ function CommentCreateForm(props) {
           />
         </InputGroup>
       </Form.Group>
+      {/* Submit button */}
       <button
         className={`${styles.Button} btn d-block ml-auto`}
         disabled={!content.trim()}
