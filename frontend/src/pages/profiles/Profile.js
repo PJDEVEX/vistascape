54deb2970029d3f05bde9653660dc3a14db3dafc
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 09:35:58 +0000

    Chro(Profile): add comments for enhanced readability

--- frontend/src/pages/profiles/Profile.js
+++ frontend/src/pages/profiles/Profile.js
@@ -7,32 +7,47 @@ import Avatar from "../../components/Avatar";
 import { Button } from "react-bootstrap";
 import { useSetProfileData } from "../../contexts/ProfileDataContext";
 
+/**
+ * Functional component representing a user profile.
+ * @param {Object} props - Component properties.
+ * @param {Object} props.profile - User profile data.
+ * @param {boolean} props.mobile - Indicates mobile view.
+ * @param {number} props.imageSize - Size of the user's avatar image.
+ */
 const Profile = (props) => {
+  // Destructuring props
   const { profile, mobile, imageSize = 55 } = props;
   const { id, following_id, image, owner } = profile;
 
+  // Accessing current user data and checking ownership
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
 
+  // Accessing functions from ProfileDataContext
   const { handleFollow, handleUnfollow } = useSetProfileData();
 
   return (
-    <div
-      className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}
-    >
+    // Main container for the profile information
+    <div className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}>
+      {/* Avatar section with a link to the user's profile */}
       <div>
         <Link className="align-self-center" to={`/profiles/${id}`}>
           <Avatar src={image} height={imageSize} />
         </Link>
       </div>
+
+      {/* Username section with a line break for smaller screens */}
       <div className={`mx-2 ${styles.WordBreak}`}>
         <strong>{owner}</strong>
       </div>
+
+      {/* Follow/Unfollow button section with conditional rendering */}
       <div className={`text-right ${!mobile && "ml-auto"}`}>
         {!mobile &&
           currentUser &&
           !is_owner &&
           (following_id ? (
+            // Unfollow button
             <Button
               className={`${btnStyles.Button} ${btnStyles.FollowToggleOutline}`}
               onClick={() => handleUnfollow(profile)}
@@ -40,6 +55,7 @@ const Profile = (props) => {
               unfollow
             </Button>
           ) : (
+            // Follow button
             <Button
               className={`${btnStyles.Button} ${btnStyles.FollowToggle}`}
               onClick={() => handleFollow(profile)}
