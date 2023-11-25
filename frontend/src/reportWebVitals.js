bfff037f69a8428e83acd9ebe1e2197e8b47665a
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 09:52:19 +0000

    Chro(reportWebVitals): add comments for enhanced readability

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,6 +1,13 @@
+/**
+ * This function reports web vital metrics to the provided callback.
+ * @param {Function} onPerfEntry - The callback function to receive performance entries.
+ */
 const reportWebVitals = (onPerfEntry) => {
+  // Check if the callback function is provided and is a valid function
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // Dynamically import the web-vitals library
     import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      // Call the callback function with Core Web Vitals metrics
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
@@ -10,4 +17,5 @@ const reportWebVitals = (onPerfEntry) => {
   }
 };
 
+// Export the reportWebVitals function as the default export of this module
 export default reportWebVitals;
