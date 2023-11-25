5d3c476ec5f2ded28ef8a9804db834f77db697fd
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 08:06:20 +0000

    Chro(Asset): add comments for enhanced readability

--- frontend/src/components/Asset.js
+++ frontend/src/components/Asset.js
@@ -2,8 +2,18 @@ import React from "react";
 import { Spinner } from "react-bootstrap";
 import styles from "../styles/Asset.module.css";
 
+/**
+ * Asset Component:
+ * A reusable component for displaying assets with optional spinner, image, and message.
+ *
+ * @param {boolean} spinner - Whether to show a loading spinner.
+ * @param {string} src - Source URL for the image.
+ * @param {string} message - Optional message to display.
+ * @returns {JSX.Element} - Asset component with dynamic content.
+ */
 const Asset = ({ spinner, src, message }) => {
   return (
+    // Asset container with Bootstrap and custom styling
     <div className={`${styles.Asset} p-4`}>
       {spinner && <Spinner animation="border" />}
       {src && <img src={src} alt={message} />}
