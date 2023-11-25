535606fa429ad2085af82976646871aae1ae17c1
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 07:42:58 +0000

    Chro(App.test): add comments for enhanced readability

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
+// TEST: App Component Rendering
+// Ensure that the App component renders without erro
+
 test("renders learn react link", () => {
   // render(<App />);
   // const linkElement = screen.getByText(/learn react/i);
