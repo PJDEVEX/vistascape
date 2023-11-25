151fa8588ce7e6e82199e55a8dd069636d74df28
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 09:07:07 +0000

    Chro(SignUpForm): add comments for enhanced readability

--- frontend/src/pages/auth/SignUpForm.js
+++ frontend/src/pages/auth/SignUpForm.js
@@ -3,27 +3,28 @@ import { Link, useHistory } from "react-router-dom";
 
 import { useColorScheme } from "../../hooks/useColorScheme";
 
+// Importing styles
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import {
-  Form,
-  Button,
-  Image,
-  Col,
-  Row,
-  Container,
-  Alert,
-} from "react-bootstrap";
+// Importing components from react-bootstrap
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
 import axios from "axios";
 import { useRedirect } from "../../hooks/useRedirect";
 
+/**
+ * Functional component representing the SignUpForm.
+ */
 const SignUpForm = () => {
+  // Hook for managing color scheme
   const { isDark } = useColorScheme();
   const darkClass = isDark ? styles["dark"] : "";
 
+  // Custom hook for redirecting if user is already logged in
   useRedirect("loggedIn");
+
+  // State for storing form data and errors
   const [signUpData, setSignUpData] = useState({
     username: "",
     password1: "",
@@ -35,6 +36,10 @@ const SignUpForm = () => {
 
   const history = useHistory();
 
+  /**
+   * Event handler for input changes.
+   * @param {Object} event - The input change event.
+   */
   const handleChange = (event) => {
     setSignUpData({
       ...signUpData,
@@ -42,23 +47,32 @@ const SignUpForm = () => {
     });
   };
 
+  /**
+   * Event handler for form submission.
+   * @param {Object} event - The form submit event.
+   */
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Sending POST request to register user
       await axios.post("/dj-rest-auth/registration/", signUpData);
       history.push("/signin");
     } catch (err) {
+      // Handling registration errors
       setErrors(err.response?.data);
     }
   };
 
   return (
     <Row className={`${styles.Row} ${darkClass}`}>
+      {/* Left column for form */}
       <Col className={`my-auto py-2 p-md-2`} md={6}>
         <Container className={`${styles.Content} ${darkClass} p-4`}>
           <h1 className={styles.Header}>sign up</h1>
 
+          {/* SignUpForm */}
           <Form onSubmit={handleSubmit}>
+            {/* Username input */}
             <Form.Group controlId="username">
               <Form.Label className="d-none">username</Form.Label>
               <Form.Control
@@ -76,6 +90,7 @@ const SignUpForm = () => {
               </Alert>
             ))}
 
+            {/* Password input */}
             <Form.Group controlId="password1">
               <Form.Label className="d-none">Password</Form.Label>
               <Form.Control
@@ -93,6 +108,7 @@ const SignUpForm = () => {
               </Alert>
             ))}
 
+            {/* Confirm password input */}
             <Form.Group controlId="password2">
               <Form.Label className="d-none">Confirm password</Form.Label>
               <Form.Control
@@ -110,6 +126,7 @@ const SignUpForm = () => {
               </Alert>
             ))}
 
+            {/* SignUp button */}
             <Button
               className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
               type="submit"
@@ -124,16 +141,16 @@ const SignUpForm = () => {
           </Form>
         </Container>
 
+        {/* Link to Sign In */}
         <Container className={`mt-3 ${styles.Content} ${darkClass}`}>
           <Link className={styles.Link} to="/signin">
             Already have an account? <span>Sign in</span>
           </Link>
         </Container>
       </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}
-      >
+
+      {/* Right column for image */}
+      <Col md={6} className={`my-auto d-none d-md-block p-2 ${styles.SignUpCol}`}>
         <Image
           className={`${appStyles.FillerImage}`}
           src={
