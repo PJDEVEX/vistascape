b3024d232fffcfcc398503ba6b285c62a36f5968
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 09:17:15 +0000

    Chro(Post): add comments for enhanced readability

--- frontend/src/pages/posts/Post.js
+++ frontend/src/pages/posts/Post.js
@@ -10,7 +10,15 @@ import { MoreDropdown } from "../../components/MoreDropdown";
 import { WhatsappShareButton, EmailShareButton } from "react-share";
 import { useColorScheme } from "../../hooks/useColorScheme";
 
+/**
+ * Component representing a single post in the application.
+ * 
+ * @component
+ * @param {Object} props - Properties passed to the component.
+ * @returns {React.Element} The rendered Post component.
+ */
 const Post = (props) => {
+  // Destructuring props for easier access
   const {
     id,
     owner,
@@ -27,21 +35,32 @@ const Post = (props) => {
     setPosts,
   } = props;
 
+  // Custom hook to get the current color scheme
   const { isDark } = useColorScheme();
   const darkClass = isDark ? styles["dark"] : "";
   const appDarkClass = isDark ? appStyles["dark"] : "";
 
+  // Custom hook to get the current user from context
   const currentUser = useCurrentUser();
+
+  // Check if the current user is the owner of the post
   const is_owner = currentUser?.username === owner;
+
+  // Check if a user is logged in
   const isUserLoggedIn = !!currentUser;
+
+  // React Router history hook for navigation
   const history = useHistory();
 
+  // State for controlling whether to show the full content of the post
   const [showFullContent, setShowFullContent] = useState(false);
 
+  // Function to handle navigation to the edit page for the current post
   const handleEdit = () => {
     history.push(`/posts/${id}/edit`);
   };
 
+  // Function to handle the deletion of the current post
   const handleDelete = async () => {
     try {
       await axiosRes.delete(`/posts/${id}/`);
@@ -51,6 +70,7 @@ const Post = (props) => {
     }
   };
 
+  // Function to handle liking a post
   const handleLike = async () => {
     try {
       const { data } = await axiosRes.post("/likes/", { post: id });
@@ -67,6 +87,7 @@ const Post = (props) => {
     }
   };
 
+  // Function to handle unliking a post
   const handleUnlike = async () => {
     try {
       await axiosRes.delete(`/likes/${like_id}/`);
@@ -83,20 +104,24 @@ const Post = (props) => {
     }
   };
 
+  // Function to toggle the display of the full content of the post
   const toggleContent = () => {
     setShowFullContent(!showFullContent);
   };
 
+  // URL for sharing the current post
   const shareUrl = `${axiosRes.defaults.baseURL}/posts/${id}/`;
 
   return (
     <Card className={`${styles.Post} ${darkClass}`}>
       <Card.Body>
         <Media className="align-items-center justify-content-between">
+          {/* Link to the profile of the post owner */}
           <Link to={`/profiles/${profile_id}`}>
             <Avatar src={profile_image} height={55} />
             {owner}
           </Link>
+          {/* Display post update time and more options for the post owner */}
           <div className="d-flex align-items-center">
             <span>{updated_at}</span>
             {is_owner && postPage && (
@@ -108,11 +133,14 @@ const Post = (props) => {
           </div>
         </Media>
       </Card.Body>
+      {/* Link to view the full post */}
       <Link to={`/posts/${id}`} className={styles.LinkStyle}>
         <Card.Img className={`${styles.ZoomedImage}`} src={image} alt={title} />
       </Link>
       <Card.Body>
+        {/* Display post title */}
         {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {/* Display post content with a "Read more" option */}
         {content && (
           <Card.Text>
             {showFullContent ? content : `${content.slice(0, 150)}...`}
@@ -123,8 +151,11 @@ const Post = (props) => {
             )}
           </Card.Text>
         )}
+        {/* Horizontal line separator */}
         <hr className={`${appStyles.HrLine} ${appDarkClass}`}/>
+        {/* Post interaction bar */}
         <div className={`${styles.PostBar} text-center`}>
+          {/* Display heart icon for post owner or provide options for liking/unliking */}
           {is_owner ? (
             <OverlayTrigger
               placement="top"
@@ -148,7 +179,9 @@ const Post = (props) => {
               <i className="far fa-heart" />
             </OverlayTrigger>
           )}
+          {/* Display the count of likes */}
           {likes_count}
+          {/* Link to view comments with comment count */}
           <Link to={`/posts/${id}`}>
             <i className="far fa-comments" />
           </Link>
@@ -157,10 +190,12 @@ const Post = (props) => {
           {/* Share Buttons */}
           {isUserLoggedIn && (
             <>
+              {/* WhatsApp share button */}
               <WhatsappShareButton url={shareUrl} title={title}>
                 <i className="fab fa-whatsapp-square" />
               </WhatsappShareButton>
 
+              {/* Email share button */}
               <EmailShareButton
                 url={shareUrl}
                 subject={title}
@@ -171,6 +206,7 @@ const Post = (props) => {
             </>
           )}
 
+          {/* Display share icons with tooltip if user is not logged in */}
           {!isUserLoggedIn && (
             <OverlayTrigger
               placement="top"
