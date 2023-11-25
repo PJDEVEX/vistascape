02e54b5d225b307748328e4b23c8ea5646c8d7c5
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 09:54:11 +0000

    Chro(SetupTests): add comments for enhanced readability

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -6,8 +6,14 @@ import "@testing-library/jest-dom";
 import { setupServer } from "msw/node";
 import { handlers } from "./mocks/handlers";
 
+// Setting up a server with the provided request handlers.
 const server = setupServer(...handlers);
 
+// Lifecycle hook: Before all tests, start the server.
 beforeAll(() => server.listen());
+
+// Lifecycle hook: After each test, reset the server handlers.
 afterEach(() => server.resetHandlers());
+
+// Lifecycle hook: After all tests, close the server.
 afterAll(() => server.close());
