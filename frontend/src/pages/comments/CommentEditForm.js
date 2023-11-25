ab94647283ac1df8b9645f1917027c30fa1cb500
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 09:12:56 +0000

    Chro(CommentEditForm): add comments for enhanced readability

--- frontend/src/pages/comments/CommentEditForm.js
+++ frontend/src/pages/comments/CommentEditForm.js
@@ -6,24 +6,41 @@ import { axiosRes } from "../../api/axiosDefaults";
 import styles from "../../styles/CommentCreateEditForm.module.css";
 import { useColorScheme } from "../../hooks/useColorScheme";
 
+/**
+ * CommentEditForm Component
+ * @param {Object} props - Component props
+ */
 function CommentEditForm(props) {
+  // Destructuring props
   const { isDark } = useColorScheme();
   const darkClass = isDark ? styles["dark"] : "";
 
   const { id, content, setShowEditForm, setComments } = props;
 
+  // State for form content
   const [formContent, setFormContent] = useState(content);
 
+  /**
+   * Handle form content change
+   * @param {Event} event - Change event
+   */
   const handleChange = (event) => {
     setFormContent(event.target.value);
   };
 
+  /**
+   * Handle form submission
+   * @param {Event} event - Form submit event
+   */
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Make API call to update comment
       await axiosRes.put(`/comments/${id}/`, {
         content: formContent.trim(),
       });
+
+      // Update comments in parent component
       setComments((prevComments) => ({
         ...prevComments,
         results: prevComments.results.map((comment) => {
@@ -36,15 +53,19 @@ function CommentEditForm(props) {
             : comment;
         }),
       }));
+
+      // Hide edit form
       setShowEditForm(false);
     } catch (err) {
       console.log(err);
     }
   };
 
+  // Render the component
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="pr-1">
+        {/* Textarea for comment content */}
         <Form.Control
           className={`${styles.Form} ${darkClass}`}
           as="textarea"
@@ -54,6 +75,7 @@ function CommentEditForm(props) {
         />
       </Form.Group>
       <div className="text-right">
+        {/* Cancel button */}
         <button
           className={styles.Button}
           onClick={() => setShowEditForm(false)}
@@ -61,6 +83,7 @@ function CommentEditForm(props) {
         >
           cancel
         </button>
+        {/* Save button */}
         <button
           className={styles.Button}
           disabled={!content.trim()}
@@ -73,4 +96,5 @@ function CommentEditForm(props) {
   );
 }
 
+// Export the component
 export default CommentEditForm;
