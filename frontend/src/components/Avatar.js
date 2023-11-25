353fef51f2f3e2d8028960ee3def219d69a219a8
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-25 08:12:12 +0000

    Chro(Avatar): add comments for enhanced readability

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -1,9 +1,18 @@
 import React from "react";
 import styles from "../styles/Avatar.module.css";
 
+/**
+ * Avatar component renders an image with optional text.
+ *
+ * @param {string} src - The source URL of the avatar image.
+ * @param {number} height - The height of the avatar image (default is 45).
+ * @param {string} text - Optional text to display alongside the avatar.
+ * @returns {JSX.Element} - The Avatar component.
+ */
 const Avatar = ({ src, height = 45, text }) => {
   return (
     <span>
+      {/* Avatar image */}
       <img
         className={styles.Avatar}
         src={src}
@@ -11,6 +20,7 @@ const Avatar = ({ src, height = 45, text }) => {
         width={height}
         alt="avatar"
       />
+      {/* Optional text */}
       {text}
     </span>
   );
