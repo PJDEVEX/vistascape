fba26c5eab005588f566a3b70d290da251b978a0
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-26 17:29:43 +0000

    Feat: Add 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import ProfilePage from "./pages/profiles/ProfilePage";
 import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import NotFound from "./components/NotFound";
 
 /**
  * Main application component.
@@ -107,7 +108,7 @@ function App() {
           />
 
           {/* 404 route */}
-          <Route render={() => <p>Page not found!</p>} />
+          <Route render={() => <NotFound/>} />
         </Switch>
       </Container>
     </div>
